91a0d415f884dec5e08ff7ec4e4e23f5ed5b7031
sakthivel.r <sakthivel.r@collabera.com>
2022-01-07 12:26:14 +0530

    "Car Singleton"

---
+++ week-1/day-5/design-patterns/src/main/java/singleton/CarSingleton.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class CarSingleton
+{
+    private static Car car;
+    private CarSingleton()
+    {
+
+    }
+    public static Car newInstance()
+    {
+        if(car==null) return car=new Car();
+        return car;
+    }
+
+}
---
+++ week-1/day-5/design-patterns/src/test/java/CarSingletonTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import singleton.CarSingleton;
+
+public class CarSingletonTests
+{
+    @DisplayName("Check Car Instance is Singleton")
+    @Test
+    void testCarSingleton()
+    {
+        var c1= CarSingleton.newInstance();
+        var c2=CarSingleton.newInstance();
+        Assertions.assertEquals(c1.hashCode(),c2.hashCode());
+    }
+}
