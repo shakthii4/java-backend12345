1db96c8569d5864fe5d889b6a925bad074bdc6e9
sakthivel.r <sakthivel.r@collabera.com>
2022-01-23 21:10:40 +0530

    "microservice "

---
+++ week-3/day-1/demo-microservice/src/main/java/com/ani/microservice/dto/Pqr.java
@@ -0,0 +1,37 @@
+package com.ani.microservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+
+public class Pqr
+{
+    private Long id;
+    private String name;
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
---
+++ week-3/day-1/demo-microservice/src/test/java/com/ani/microservice/DemoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
