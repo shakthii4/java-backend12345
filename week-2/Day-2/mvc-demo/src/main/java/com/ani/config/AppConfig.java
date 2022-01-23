961e10e720e419f763b10af6b856222bd86e095f
sakthivel.r <sakthivel.r@collabera.com>
2022-01-23 19:06:37 +0530

    "Spring Mvc"

---
+++ week-2/Day-2/mvc-demo/src/main/java/com/ani/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.ani.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ani", "com.ani.controller"})
+@Configuration
+public class AppConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setViewClass(JstlView.class);
+        vr.setPrefix("/"); // /data.jsp
+        vr.setSuffix(".jsp");
+        return vr;
+    }
+
+}
--- week-2/Day-2/mvc-demo/src/main/java/com/ani/controller/HellowController.java
+++ week-2/Day-2/mvc-demo/src/main/java/com/ani/controller/HellowController.java
@@ -2,6 +2,8 @@ package com.ani.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HellowController
@@ -10,6 +12,15 @@ public class HellowController
     public String login()
     {
         return "login";
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hi() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/dt", method = RequestMethod.GET)
+    public String data() {
+        return "data";
     }
 
 }
